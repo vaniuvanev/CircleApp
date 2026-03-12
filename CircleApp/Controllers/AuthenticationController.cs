fbb584a36b85153b649a568f20ac2dca309cb29e
vaniuvanev <vantodoro@gmail.com>
2026-03-12 20:43:45 +0200

    added admin dashboard

--- CircleApp/Controllers/AuthenticationController.cs
+++ CircleApp/Controllers/AuthenticationController.cs
@@ -47,7 +47,12 @@ namespace CircleApp.Controllers
                 false);
 
             if (result.Succeeded)
-                return RedirectToAction("Index", "Home");
+            {
+                if (User.IsInRole(AppRoles.Admin))
+                    return RedirectToAction("Index", "Admin");
+                else
+                    return RedirectToAction("Index", "Home");
+            }
 
             ModelState.AddModelError("", "Invalid login attempt");
             return View(loginVM);
--- CircleApp/Program.cs
+++ CircleApp/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddScoped<IStoriesService, StoriesService>();
 builder.Services.AddScoped<IFilesService, FilesService>();
 builder.Services.AddScoped<IUsersService, UsersService>();
 builder.Services.AddScoped<IFriendsService, FriendsService>();
+builder.Services.AddScoped<IAdminService, AdminService>();
 
 // Identity
 builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
